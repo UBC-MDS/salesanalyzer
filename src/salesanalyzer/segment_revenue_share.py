6bfc3efdd69e5d5dc136235c344456fb6c5d0c53
Franklin Aryee <faryee@student.ubc.ca>
2025-01-17 15:40:42 -0800

    feat: updated segment_revenue_share function

--- src/salesanalyzer/segment_revenue_share.py
+++ src/salesanalyzer/segment_revenue_share.py
@@ -1,7 +1,12 @@
-def segment_revenue_share(sales_data, price_col='UnitPrice', quantity_col='Quantity'):
+import pandas as pd
+
+
+def segment_revenue_share(sales_data,
+                          price_col='UnitPrice',
+                          quantity_col='Quantity'):
     """
-    Segments products into three categories—cheap, medium, and expensive—based 
-    on price, and calculates their respective share in total revenue.
+    Segments products into three categories—cheap, medium, and expensive—
+    based on price, and calculates their respective share in total revenue.
 
     Parameters:
     -----------
@@ -15,9 +20,39 @@ def segment_revenue_share(sales_data, price_col='UnitPrice', quantity_col='Quant
     Returns:
     --------
     pd.DataFrame
-        A DataFrame showing the total revenue share for each price segment: 
+        A DataFrame showing the total revenue share for each price segment:
         'cheap', 'medium', 'expensive'.
+
+    Raises:
+    -------
+    ValueError:
+        If the input DataFrame is empty or specified columns contain
+        any missing data
+    KeyError:
+        If any of the specified columns are missing in the DataFrame.
+    TypeError:
+        If any of the columns contain invalid data types.
     """
+    # Check if input dataframe is empty
+    if sales_data.empty:
+        raise ValueError("Input DataFrame is empty.")
+
+    # Check if required columns are missing
+    required_columns = {price_col, quantity_col}
+    missing_columns = required_columns - set(sales_data.columns)
+    if missing_columns:
+        raise KeyError(f"Missing columns in input DataFrame: {missing_columns}")
+
+    # Check if required columns contain any missing data
+    if sales_data[price_col].isna().any() or sales_data[quantity_col].isna().any():
+        raise ValueError(f"{price_col} or {quantity_col} column contains missing values.")
+
+    # Check if invalid data types are present in required columns
+    if not pd.api.types.is_numeric_dtype(sales_data[price_col]):
+        raise TypeError(f"{price_col} must contain numeric data.")
+    if not pd.api.types.is_numeric_dtype(sales_data[quantity_col]):
+        raise TypeError(f"{quantity_col} must contain numeric data.")
+
     # Calculate revenue as price * quantity
     sales_data['Revenue'] = sales_data[price_col] * sales_data[quantity_col]
 
